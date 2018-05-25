ec750dd680f9d1d7e6bad95274b4f16f322468b3
n.yerdenqyzy <n.yerdenqyzy@bugingroup.kz>
2018-05-25 16:03:29 +0600

    Signed-off-by: n.yerdenqyzy <n.yerdenqyzy@bugingroup.kz>

--- js/common.js
+++ js/common.js
@@ -27,3 +27,13 @@ jQuery(function(){
         thumbnails: true
     });
 });
+(function($){
+    $(document).ready(function(){
+        $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            $(this).parent().siblings().removeClass('open');
+            $(this).parent().toggleClass('open');
+        });
+    });
+})(jQuery);
